4009dfeea064bab84e48d4c564c3bddca48a0418
Rickard Marjanovic <rickard.marjanovic@gmail.com>
2019-07-24 14:24:37 +0200

    Solved exercise

--- data-type-determinator/src/determinator.js
+++ data-type-determinator/src/determinator.js
@@ -18,8 +18,25 @@
 function tellType (data) {
   let result = 'You sent me '
 
-  // TODO: Write your code here.
-
+  if (typeof (data) === 'number') {
+    result += 'a number.'
+  } else if (typeof (data) === 'string') {
+    result += 'a string.'
+  } else if (typeof (data) === 'boolean') {
+    result += 'a boolean.'
+  } else if (typeof (data) === 'object') {
+    if (data === null) {
+      result += 'a null value.'
+    } else if (Array.isArray(data) === true) {
+      result += 'an array.'
+    } else {
+      result += 'an object.'
+    }
+  } else if (typeof (data) === 'function') {
+    result += 'a function.'
+  } else if (typeof (data) === 'undefined') {
+    result += 'an undefined value.'
+  }
   return result
 }
 
