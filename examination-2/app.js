819a75cfcd911fc3c59dd8dd6059ad120aacfab8
Rickard Marjanovic <rickard.marjanovic@gmail.com>
2019-08-16 17:14:48 +0200

    Changed shuffle method to use this.deck and created handCard method

--- examination-2/app.js
+++ examination-2/app.js
@@ -2,10 +2,21 @@
 
 const Deck = require('./src/Deck')
 
-const deck = new Deck()
+let deck = new Deck()
 
-const deck2 = deck.createDeck()
+let deck2 = deck.createDeck()
 
-let deck3 = deck.shuffleDeck(deck2)
+console.log(deck2)
+console.log(deck2.length)
 
-console.log(deck3)
+deck2 = deck.shuffleDeck()
+
+console.log(deck2)
+console.log(deck2.length)
+
+let card = deck.handCard()
+
+console.log(card)
+
+console.log(deck2)
+console.log(deck2.length)
--- examination-2/src/Deck.js
+++ examination-2/src/Deck.js
@@ -5,25 +5,24 @@ class Deck {
     this.deck = [] // created an array to hold the deck
   }
 
-  shuffleDeck (array) {
-    let newArray = array.slice(0, array.length)
+  shuffleDeck () {
     let tempValue, randomNo
-    let currentElement = newArray.length
+    let currentElement = this.deck.length
 
     while (currentElement !== 0) {
       randomNo = Math.floor(Math.random() * (currentElement))
 
       currentElement--
 
-      tempValue = newArray[randomNo]
-      newArray[randomNo] = newArray[currentElement]
-      newArray[currentElement] = tempValue
+      tempValue = this.deck[randomNo]
+      this.deck[randomNo] = this.deck[currentElement]
+      this.deck[currentElement] = tempValue
     }
-    return newArray
+    return this.deck
   }
 
   handCard () {
-
+    return this.deck.pop()
   }
 
   createDeck () {
